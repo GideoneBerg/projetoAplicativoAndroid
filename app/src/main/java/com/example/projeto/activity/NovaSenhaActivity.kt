60f1ba9e2a0105aa0ee450da4a3f511c3ddddfdf
Gideone_Berg <114090403+GideoneBerg@users.noreply.github.com>
2023-09-21 17:26:10 -0300

    alterando código java para kotlin

--- app/src/main/java/com/example/projeto/activity/NovaSenhaActivity.kt
+++ app/src/main/java/com/example/projeto/activity/NovaSenhaActivity.kt
@@ -1,91 +1,64 @@
-package com.example.projeto.activity;
+package com.example.projeto.activity
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.projeto.R
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.RetryPolicy;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.projeto.R;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class NovaSenhaActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nova_senha);
-        String email = getIntent().getExtras().getString("email");
-        EditText editTextNewPassword = findViewById(R.id.new_password);
-        EditText editTextOPT = findViewById(R.id.otp);
-        ProgressBar progressBar = findViewById(R.id.progressBar);
-        Button button = findViewById(R.id.btnEnviar);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                progressBar.setVisibility(View.VISIBLE);
-                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                String url = "http://192.168.31.75/Login/new_password.php";
-
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                progressBar.setVisibility(View.GONE);
-                                if (response.equals("success")) {
-                                    Toast.makeText(getApplicationContext(), "Nova Senha Definida com Sucesso", Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-
-                                    startActivity(intent);
-                                    finish();
-
-                                } else
-                                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
-
-
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        progressBar.setVisibility(View.GONE);
-                        error.printStackTrace();
-
-                    }
+class NovaSenhaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nova_senha)
+        val email = intent.extras!!.getString("email")
+        val editTextNewPassword = findViewById<EditText>(R.id.new_password)
+        val editTextOPT = findViewById<EditText>(R.id.otp)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val button = findViewById<Button>(R.id.btnEnviar)
+        button.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            val queue = Volley.newRequestQueue(applicationContext)
+            val url = "http://192.168.31.75/Login/new_password.php"
+            val stringRequest: StringRequest = object : StringRequest(
+                Method.POST, url,
+                Response.Listener<String> { response ->
+                    progressBar.visibility = View.GONE
+                    if (response == "success") {
+                        Toast.makeText(
+                            applicationContext,
+                            "Nova Senha Definida com Sucesso",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val intent = Intent(applicationContext, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else Toast.makeText(applicationContext, response, Toast.LENGTH_SHORT).show()
+                }, Response.ErrorListener { error ->
+                    progressBar.visibility = View.GONE
+                    error.printStackTrace()
                 }) {
-                    protected Map<String, String> getParams() {
-                        Map<String, String> paramV = new HashMap<>();
-                        paramV.put("email", email);
-                        paramV.put("otp", editTextOPT.getText().toString());
-                        paramV.put("new-password", editTextNewPassword.getText().toString());
-                        return paramV;
-                    }
-                };
-
-                stringRequest.setRetryPolicy(new DefaultRetryPolicy(
-                        5000, // timeout em milissegundos
-                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
-                ));
-                queue.add(stringRequest);
-
+                override fun getParams(): Map<String, String>? {
+                    val paramV: MutableMap<String, String> = HashMap()
+                    paramV["email"] = email!!
+                    paramV["otp"] = editTextOPT.text.toString()
+                    paramV["new-password"] = editTextNewPassword.text.toString()
+                    return paramV
+                }
             }
-
-        });
+            stringRequest.retryPolicy = DefaultRetryPolicy(
+                5000,  // timeout em milissegundos
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+            )
+            queue.add(stringRequest)
+        }
     }
-
 }
--- app/src/main/java/com/example/projeto/activity/RedefinirSenhaActivity.kt
+++ app/src/main/java/com/example/projeto/activity/RedefinirSenhaActivity.kt
@@ -1,116 +1,84 @@
-package com.example.projeto.activity;
+package com.example.projeto.activity
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.RetryPolicy
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.projeto.R
+import java.util.regex.Pattern
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.RetryPolicy;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import com.example.projeto.R;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class RedefinirSenhaActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_redefinir_senha);
-        EditText editText = findViewById(R.id.email);
-        Button button = findViewById(R.id.btnEnviar);
-        ProgressBar progressBar = findViewById(R.id.progressBar);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = editText.getText().toString();
-
-                if (!isValidEmail(email)) {
-                    // Exibir uma mensagem de erro ao usuário
-                    editText.setError("Email inválido");
-                    return;
-                }
-                progressBar.setVisibility(View.VISIBLE);
-                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                String url ="http://192.168.31.75/Login/reset_password.php";
-
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                progressBar.setVisibility(View.GONE);
-                                if(response.equals("success")){
-                                    Intent intent = new Intent(getApplicationContext(), NovaSenhaActivity.class);
-                                    intent.putExtra("email", editText.getText().toString());
-                                    startActivity(intent);
-                                    finish();
-
-                                }
-                                else
-                                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
-
-
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        progressBar.setVisibility(View.GONE);
-                        error.printStackTrace();
-
-                    }
-                }){
-                    protected Map<String, String> getParams(){
-                        Map<String, String> paramV = new HashMap<>();
-                        paramV.put("email", editText.getText().toString());
-                        return paramV;
-                    }
-                };
-                // definindo um tempo para verificar o email
-                stringRequest.setRetryPolicy(new RetryPolicy() {
-                    @Override
-                    public int getCurrentTimeout() {
-                        return 30000;
-                    }
-
-                    @Override
-                    public int getCurrentRetryCount() {
-                        return 30000;
-                    }
-
-                    @Override
-                    public void retry(VolleyError error) throws VolleyError {
-
-                    }
-                });
-                queue.add(stringRequest);
-
+class RedefinirSenhaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_redefinir_senha)
+        val editText = findViewById<EditText>(R.id.email)
+        val button = findViewById<Button>(R.id.btnEnviar)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        button.setOnClickListener(View.OnClickListener {
+            val email = editText.text.toString()
+            if (!isValidEmail(email)) {
+                // Exibir uma mensagem de erro ao usuário
+                editText.error = "Email inválido"
+                return@OnClickListener
             }
-   });
-}
+            progressBar.visibility = View.VISIBLE
+            val queue = Volley.newRequestQueue(applicationContext)
+            val url = "http://192.168.31.75/Login/reset_password.php"
+            val stringRequest: StringRequest = object : StringRequest(
+                Method.POST, url,
+                Response.Listener<String> { response ->
+                    progressBar.visibility = View.GONE
+                    if (response == "success") {
+                        val intent = Intent(applicationContext, NovaSenhaActivity::class.java)
+                        intent.putExtra("email", editText.text.toString())
+                        startActivity(intent)
+                        finish()
+                    } else Toast.makeText(applicationContext, response, Toast.LENGTH_SHORT).show()
+                }, Response.ErrorListener { error ->
+                    progressBar.visibility = View.GONE
+                    error.printStackTrace()
+                }) {
+                override fun getParams(): Map<String, String>? {
+                    val paramV: MutableMap<String, String> = HashMap()
+                    paramV["email"] = editText.text.toString()
+                    return paramV
+                }
+            }
+            // definindo um tempo para verificar o email
+            stringRequest.retryPolicy = object : RetryPolicy {
+                override fun getCurrentTimeout(): Int {
+                    return 30000
+                }
 
-    private boolean isValidEmail(CharSequence email) {
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+                override fun getCurrentRetryCount(): Int {
+                    return 30000
+                }
+
+                @Throws(VolleyError::class)
+                override fun retry(error: VolleyError) {
+                }
+            }
+            queue.add(stringRequest)
+        })
     }
 
-    private static final String EMAIL_PATTERN =
-            "[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}";
+    private fun isValidEmail(email: CharSequence): Boolean {
+        val pattern = Pattern.compile(EMAIL_PATTERN)
+        val matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
 
-
-
+    companion object {
+        // mascara do email
+        private const val EMAIL_PATTERN = "[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}"
+    }
 }
