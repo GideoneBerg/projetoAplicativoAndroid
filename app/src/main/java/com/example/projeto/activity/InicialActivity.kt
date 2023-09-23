b4a9bafff864e945a53f90956acce2eae287d8cb
Gideone_Berg <114090403+GideoneBerg@users.noreply.github.com>
2023-09-23 14:27:16 -0300

    algumas mudanças

--- app/src/main/java/com/example/projeto/activity/InicialActivity.kt
+++ app/src/main/java/com/example/projeto/activity/InicialActivity.kt
@@ -7,31 +7,43 @@ import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projeto.R
+import com.example.projeto.databinding.ActivityInicialBinding
 
 class InicialActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInicialBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val whatsapp =
-            "https://api.whatsapp.com/send/?phone=5581986271986&text&type=phone_number&app_absent=0"
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inicial)
-        val botaoWeb = findViewById<View>(R.id.button_newCliente) as Button
-        botaoWeb.setOnClickListener { view: View? ->
-            startActivity(
-                Intent(
-                    Intent.ACTION_VIEW,
-                    Uri.parse(whatsapp)
-                )
-            )
+        binding = ActivityInicialBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        botoes()
+
+    }
+
+    fun botoes(){
+
+        binding.buttonCliente.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.buttonPlanos.setOnClickListener {
+            val intent = Intent(this, PlanosActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.buttonNewCliente.setOnClickListener {
+            val whatsapp =
+                "https://api.whatsapp.com/send/?phone=5581986271986&text&type=phone_number&app_absent=0"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(whatsapp))
+            startActivity(intent)
+
         }
     }
 
-    fun button_cliente(a: View?) {
-        val i = Intent(this, LoginActivity::class.java)
-        startActivity(i)
-    }
-
-    fun button_planos(b: View?) {
-        val i = Intent(this, PlanosActivity::class.java)
-        startActivity(i)
-    }
 }
